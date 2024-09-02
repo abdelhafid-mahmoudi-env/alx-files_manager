b8d0f51caa6e5d76f8533675f1ebad61010a7d3c
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-09-02 07:12:37 -0700

    test

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,18 @@
-import { createClient } from 'redis';
+import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => console.error('Redis client error', err));
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+      // console.log('connected to the server');
+    });
   }
 
   isAlive() {
@@ -11,17 +20,12 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, val) => {
-        if (err) reject(err);
-        resolve(val);
-      });
-    });
+    const value = await this.getAsync(key);
+    return value;
   }
 
   async set(key, value, duration) {
-    this.client.set(key, value);
-    this.client.expire(key, duration);
+    this.client.setex(key, duration, value);
   }
 
   async del(key) {
@@ -30,4 +34,5 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
+
 export default redisClient;
