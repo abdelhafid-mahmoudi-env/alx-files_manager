fc540e101913455d7775a3283b41e81b2fedb243
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-09-03 14:35:40 -0700

    test

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,26 +7,35 @@ class AuthController {
   static async getConnect(req, res) {
     const auth = req.headers.authorization;
     if (!auth) return res.status(401).json({ error: 'Unauthorized' });
-
-    const base64Credentials = auth.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const [email, password] = credentials.split(':');
-
-    const user = await dbClient.db.collection('users').findOne({ email, password: sha1(password) });
-    if (!user) return res.status(401).json({ error: 'Unauthorized' });
-
-    const token = uuidv4();
-    await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
-
-    return res.status(200).json({ token });
+    try {
+      const base64Credentials = auth.split(' ')[1];
+      const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+      const [email, password] = credentials.split(':');
+      const hashedPassword = sha1(password);
+      const user = await dbClient.db.collection('users').findOne({ email, password: hashedPassword });
+      if (!user) return res.status(401).json({ error: 'Unauthorized' });
+      const token = uuidv4();
+      await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.error('Error during user authentication:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
     if (!token) return res.status(401).json({ error: 'Unauthorized' });
-
-    await redisClient.del(`auth_${token}`);
-    return res.status(204).send();
+    try {
+      const deleted = await redisClient.del(`auth_${token}`);
+      if (!deleted) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      return res.status(204).send();
+    } catch (error) {
+      console.error('Error during user sign-out:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
